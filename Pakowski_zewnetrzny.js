54b722cdfb85b091ff82d18f5da0239d6ae08b0e
MarekPakow <marekpakowski10@gmail.com>
2025-09-04 10:35:45 +0200

    Add files via upload

---
+++ Pakowski_zewnetrzny.js
@@ -0,0 +1,2 @@
+document.write("ostatnia modyfikacja strony".bold().strike().fontcolor("olive").fontsize(5)+"<br>"); 
+document.write(document.lastModified .bold().fontcolor("lime").fontsize(5)+"<br>"); 
